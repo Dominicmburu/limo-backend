02b59b335903e71201321e12e5d0c4980ab932e4
Dominicmburu <dominicmburu034@gmail.com>
2025-04-28 12:54:45 +0300

    connection string

--- src/data-source.js
+++ src/data-source.js
@@ -7,9 +7,11 @@ const { Promotion } = require('./entities/Promotion');
 const { Vehicle } = require('./entities/Vehicle');
 require('dotenv').config();
 
+postgresql://dominic:WCArBz4DgGwnOVVB2AGBvNwB6oOR4rHK@dpg-d07k2opr0fns738l454g-a/limodb_cf62
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'dpg-d07k2opr0fns738l454g-a.oregon-postgres.render.com',
+  host: process.env.DB_HOST || 'dpg-d07k2opr0fns738l454g-a',
+  // host: process.env.DB_HOST || 'dpg-d07k2opr0fns738l454g-a.oregon-postgres.render.com',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   username: process.env.DB_USERNAME || 'dominic',
   password: process.env.DB_PASSWORD || 'WCArBz4DgGwnOVVB2AGBvNwB6oOR4rHK',
